45e6406e627bd557b69b2a91cf6dd9418cff3d17
Zsolt Toth <tothzs87@gmail.com>
2022-11-09 10:31:06 +0100

    chore: remove temple files

--- src/main/java/hu/tzs/employee/dao/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package hu.tzs.employee.dao;
-
-import hu.tzs.employee.dao.entity.BookEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<BookEntity, String> {
-}
