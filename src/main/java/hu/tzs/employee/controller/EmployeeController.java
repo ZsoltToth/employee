6234a2df59a0885f85a0f9fe6b6537898eec9a2d
Zsolt Toth <tothzs87@gmail.com>
2022-11-11 07:51:18 +0100

    chore: hide dummy data into service from controller

--- src/main/java/hu/tzs/employee/controller/EmployeeController.java
+++ src/main/java/hu/tzs/employee/controller/EmployeeController.java
@@ -1,7 +1,6 @@
 package hu.tzs.employee.controller;
 
 import hu.tzs.employee.controller.dto.EmployeeDto;
-import hu.tzs.employee.model.Gender;
 import hu.tzs.employee.service.EmployeeManagerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,36 +8,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
-import java.util.Date;
-import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/employees")
 @RequiredArgsConstructor
 public class EmployeeController {
 
-//    private final EmployeeManagerService employeeManagerService;
+    private final EmployeeManagerService employeeManagerService;
 
     @GetMapping("")
-    public Collection<EmployeeDto> fetchAll(){
-        EmployeeDto alice = new EmployeeDto();
-        alice.setEmpNo(0);
-        alice.setFirstName("Alice");
-        alice.setLastName("Doe");
-        alice.setGender(Gender.FEMALE.getValue());
-        alice.setHireDate("2000-01-02");
-        alice.setSalary(0);
-        alice.setTitle("Test Object");
-        EmployeeDto bob = new EmployeeDto();
-        bob.setEmpNo(0);
-        bob.setFirstName("Bob");
-        bob.setLastName("Smith");
-        bob.setGender(Gender.MALE.getValue());
-        bob.setHireDate("2010-12-02");
-        bob.setSalary(0);
-        bob.setTitle("Test Object");
-       return List.of(
-           alice, bob
-       );
+    public Collection<EmployeeDto> fetchAll() {
+        return employeeManagerService.getEmployees().stream().map(employee ->
+            EmployeeDto.builder()
+                .empNo(employee.getEmpNo())
+                .firstName(employee.getFirstName())
+                .lastName(employee.getLastName())
+                .gender(employee.getGender().getValue())
+                .hireDate(employee.getHireDate().toString())
+                .salary(employee.getSalary())
+                .title(employee.getTitle())
+                .build()
+        ).collect(Collectors.toList());
     }
 }
--- src/main/java/hu/tzs/employee/controller/dto/EmployeeDto.java
+++ src/main/java/hu/tzs/employee/controller/dto/EmployeeDto.java
@@ -1,11 +1,13 @@
 package hu.tzs.employee.controller.dto;
 
 import hu.tzs.employee.model.Gender;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@Builder
 public class EmployeeDto {
 
     private int empNo;
