c33b6d3ca9f07c605c9320cf001a28668c16e98b
Zsolt Toth <tothzs87@gmail.com>
2023-07-25 07:28:50 +0200

    feat: add fetch employee by id endpoint
    Ref.: #5

--- src/main/java/hu/tzs/employee/controller/EmployeeController.java
+++ src/main/java/hu/tzs/employee/controller/EmployeeController.java
@@ -2,10 +2,15 @@ package hu.tzs.employee.controller;
 
 import hu.tzs.employee.controller.dto.EmployeeDto;
 import hu.tzs.employee.service.EmployeeManagerService;
+import hu.tzs.employee.service.exception.EmployeeNotFoundException;
+import hu.tzs.employee.service.model.Employee;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 import java.util.stream.Collectors;
@@ -19,17 +24,28 @@ public class EmployeeController {
 
     @GetMapping("")
     public Collection<EmployeeDto> fetchAll() {
-        return employeeManagerService.getEmployees().stream().map(employee ->
-            EmployeeDto.builder()
-                .empNo(employee.getEmpNo())
-                .firstName(employee.getFirstName())
-                .lastName(employee.getLastName())
-                .gender(employee.getGender().getValue())
-                .hireDate(employee.getHireDate().toString())
-                .salary(employee.getSalary())
-                .title(employee.getTitle())
-                .department(employee.getDepartment() != null ? employee.getDepartment().getName() : "")
-                .build()
-        ).collect(Collectors.toList());
+        return employeeManagerService.getEmployees().stream().map(this::mapEmployeeToEmployeeDto).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{empNo}")
+    public EmployeeDto fetchEmployee(@RequestParam int empNo){
+        try{
+            return mapEmployeeToEmployeeDto(employeeManagerService.getEmployee(empNo));
+        } catch (EmployeeNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,e.getMessage());
+        }
+    }
+
+    private EmployeeDto mapEmployeeToEmployeeDto(Employee employee){
+        return EmployeeDto.builder()
+            .empNo(employee.getEmpNo())
+            .firstName(employee.getFirstName())
+            .lastName(employee.getLastName())
+            .gender(employee.getGender().getValue())
+            .hireDate(employee.getHireDate().toString())
+            .salary(employee.getSalary())
+            .title(employee.getTitle())
+            .department(employee.getDepartment() != null ? employee.getDepartment().getName() : "")
+            .build();
     }
 }
--- src/main/java/hu/tzs/employee/persist/EmployeeDao.java
+++ src/main/java/hu/tzs/employee/persist/EmployeeDao.java
@@ -1,5 +1,6 @@
 package hu.tzs.employee.persist;
 
+import hu.tzs.employee.service.exception.EmployeeNotFoundException;
 import hu.tzs.employee.service.model.Employee;
 
 import java.util.Collection;
@@ -7,4 +8,6 @@ import java.util.Collection;
 public interface EmployeeDao {
 
     Collection<Employee> getEmployees();
+
+    Employee getEmployee(int empNo) throws EmployeeNotFoundException;
 }
